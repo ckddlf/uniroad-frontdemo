ea36749aad89d6cfefbc1db7d5188a688588a010
ckddlf <53569322+ckddlf@users.noreply.github.com>
2026-05-04 14:54:53 +0900

    Update page.tsx

--- src/app/landing/page.tsx
+++ src/app/landing/page.tsx
@@ -103,7 +103,7 @@ export default function LandingPage() {
 
           <div className="registration-count">
             <span className="dot"></span>
-            현재 <span className="count">22</span> 명이 사전 등록했습니다
+            현재 <span className="count">17</span> 명이 사전 등록했습니다
           </div>
         </div>
       </section>
